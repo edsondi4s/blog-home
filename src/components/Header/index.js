9b034e2ab78689f90043bd900c0405c3ad3961d9
edsigner <edson.designer2015@hotmail.com>
2022-04-23 13:46:27 -0300

    Styling the menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,31 @@
+import logo from '../../svg/logotipo.svg';
+
 const Header = () => {
+
+    const desclogo= "Essa é a logo";
+
     return(
-        <h1>Header.js</h1>
+        <>
+            <header  className='flex-space-between'>
+                <div className="logo">
+                    <img src={logo} alt={desclogo}/>
+                </div>
+                <div className="search">
+                    <input type="text" className='input-search' name="search"/>
+                </div>
+                <ul className="menu">
+                    <li>
+                        <a href='#' className='nav-link'>Categories</a>
+                    </li>
+                    <li>
+                        <a href='#' className='nav-link'>About</a>
+                    </li>
+                    <li>
+                        <a href='#' className='nav-link'>Contact</a>
+                    </li>
+                </ul>
+            </header>
+        </>
     );
 }
 export default Header;
